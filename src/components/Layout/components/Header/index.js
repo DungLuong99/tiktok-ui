4e1a27ca50d1e3c488e010788b291da57477a85c
DungLuong99 <dunglt2301@gmail.com>
2022-07-01 15:53:08 +0700

    xay dung ui header 1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,6 +1,7 @@
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import Tippy from '@tippyjs/react/headless';
 
 import styles from './Header.module.scss';
 import images from '~/assets/images';
@@ -19,9 +20,19 @@ function Header() {
                     <FontAwesomeIcon icon={faCircleXmark} />
                 </button>
                 <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-                <button className={cx('search-btn')}>
-                    <FontAwesomeIcon icon={faMagnifyingGlass} />
-                </button>
+
+
+                <Tippy
+                    render = {attrs => (
+                        <div className = {cx('search-result')} tabIndex="-1" {...attrs}>
+                            ket qua
+                        </div>
+                    )}
+                >
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </Tippy>
             </div>
             <div className= {cx('action')}>
 
