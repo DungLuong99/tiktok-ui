5ebcc4223fffc24425157399c1411fc13f8fa14c
DungLuong99 <dunglt2301@gmail.com>
2022-07-01 15:54:22 +0700

    UI #2

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,21 @@
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames/bind";
+import style from './AccountItem.module.scss'
+
+const cx = classNames.bind(style);
+
+function AccountItem() {
+    return ( <div className={cx('wrapper')}>
+        <img className={cx('avatar')} src = '' alt="Name" />
+        <div className={cx('info')}>
+            <h4 className={cx('name')}>
+                <span>N.V.A</span>
+                <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+            </h4>
+            <span className={cx('username')}>N.V.A </span>
+        </div>
+    </div> );
+}
+
+export default AccountItem;
