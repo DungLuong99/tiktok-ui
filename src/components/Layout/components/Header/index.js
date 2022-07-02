f5b58b64620c582752aa98347a1a67a262d1c3ed
DungLuong99 <dunglt2301@gmail.com>
2022-07-02 23:03:16 +0700

    ui header #4

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { faCircle, faCircleQuestion, faCircleXmark, faEarthAsia, faEllipsisVertical, faKeyboard, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react/headless';
 
 import Button from '~/components/Button';
@@ -9,17 +9,34 @@ import { Wrapper as PopperWrapper } from '~/components/Popper';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 import AccountItem from '~/components/AccountItem';
+import Menu from '~/components/Popper/Menu';
 
 const cx = classNames.bind(styles);
+
+const MENU_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faEarthAsia} />,
+        title: 'English',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+        title: 'Feedback and help',
+        to: '/feedback',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faKeyboard} />,
+        title: 'Keyboard shortcuts',
+    },
+]
 
 function Header() {
     const [searchResult, setSearchResult] = useState([]);
 
-    useEffect(()=>{
-        setTimeout(()=> {
+    useEffect(() => {
+        setTimeout(() => {
             setSearchResult([]);
-        },0)
-    },[])    
+        }, 0)
+    }, [])
 
 
     return <header className={cx('wrapper')} >
@@ -28,6 +45,7 @@ function Header() {
                 <img src={images.logo} alt='Tiktok' />
             </div>
             <Tippy
+                interactive
                 visible={searchResult.length > 0}
                 render={attrs => (
                     <div className={cx('search-result')} tabIndex="-1" {...attrs}>
@@ -35,7 +53,7 @@ function Header() {
                             <h4 className={cx('search-title')}>
                                 Accounts
                             </h4>
-                            <AccountItem/>
+                            <AccountItem />
                         </PopperWrapper>
                     </div>
                 )}
@@ -52,10 +70,17 @@ function Header() {
                     </button>
                 </div>
             </Tippy>
+
             <div className={cx('action')}>
+
                 <Button text >Upload</Button>
                 <Button primary>Login</Button>
 
+                <Menu items={MENU_ITEMS}>
+                    <button className={cx('more-btn')}>
+                        <FontAwesomeIcon icon={faEllipsisVertical} />
+                    </button>
+                </Menu>
             </div>
         </div>
     </header>
